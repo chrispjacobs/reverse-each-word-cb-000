02153d3cec7ccad6cf24b6cd17202f40086b96b3
Christopher Jacobs <chrisj.3222@gmail.com>
2019-02-28 00:28:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   sentence_array.collect do |word|
     word.reverse
   end
-  altered_array.join(" ")
+  sentence_array.join(" ")
 end
 
 puts reverse_each_word("This is a sentence.")
